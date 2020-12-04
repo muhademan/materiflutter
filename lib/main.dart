015ed408864241627059bfc8dda42d3f86816637
muhademan <muhademan>
2020-12-04 16:02:01 +0700

    row widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,30 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Belajar Flutter'),
+        title: Text('Belajar Layout'),
       ),
-      body: Container(
-          decoration: BoxDecoration(
-            color: const Color(0xff7c94b6),
-            image: const DecorationImage(
-              image: NetworkImage('https://i.pinimg.com/originals/91/86/6b/91866b918c9cca0747f483a46943e926.jpg'),
-              fit: BoxFit.cover,
+      body: Row(
+        children: [
+          Container(
+            color: Colors.greenAccent,
+            child: FlutterLogo(
+              size: 60.0,
             ),
-            border: Border.all(
-              color: Colors.black,
-              width: 8,
-            ),
-            borderRadius: BorderRadius.circular(12),
           ),
-          height: 280,
-          width: 200,
-          margin: EdgeInsets.all(20)
-            )
-          );
+          Container(
+            color: Colors.orangeAccent,
+            child: FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+          Container(
+            color: Colors.purpleAccent,
+            child: FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
