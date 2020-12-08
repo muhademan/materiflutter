2132d489cd3d77aa61f14523f169f556a48b71b6
muhademan <ade11rahman@gmail.com>
2020-12-08 09:36:15 +0700

    Toolbar Dinamis dengan SliverAppBar

--- lib/main.dart
+++ lib/main.dart
@@ -8,29 +8,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: ContohDynamicAppBar(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class ContohDynamicAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ade Rahman'),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                    Colors.green,
-                    Colors.blue
-                  ])
-          ),
+    return SafeArea(
+      child: Material(
+        child: CustomScrollView(
+          slivers: [
+            SliverPersistentHeader(
+              delegate: MySliverAppBar(expandedHeight: 200),
+              pinned: true,
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                    (_, index) => ListTile(
+                  title: Text("Song $index"),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+}
+
+class MySliverAppBar extends SliverPersistentHeaderDelegate {
+  final double expandedHeight;
+
+  MySliverAppBar({@required this.expandedHeight});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Stack(
+      fit: StackFit.expand,
+      overflow: Overflow.visible,
+      children: [
+        Image(
+          image: AssetImage('assets/images/Babymetal-Metal-Galaxy.jpg'),
+          fit: BoxFit.cover,
+        ),
+        Center(
+          child: Opacity(
+            opacity: shrinkOffset / expandedHeight,
+            child: Text(
+              "BabyMetal di BelajarFlutter",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: expandedHeight / 2 - shrinkOffset,
+          left: MediaQuery.of(context).size.width / 4,
+          child: Opacity(
+            opacity: (1 - shrinkOffset / expandedHeight),
+            child: ClipOval(
+              child: Image(
+                width: MediaQuery.of(context).size.width / 2,
+                image: AssetImage('assets/images/babymetal-1.png'),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => expandedHeight;
+
+  @override
+  double get minExtent => kToolbarHeight;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
 }
